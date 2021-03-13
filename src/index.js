78f4a4c1cc96b6749233b6c0664c14d0af2d68fd
SteunHaus <pavel.evdokimenko@gmail.com>
2021-03-13 12:39:13 +0300

    fix: Updage index.js with valid solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = String(n);
+  let reverseStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[str.length-i-1] != "-") {
+        reverseStr = reverseStr + str[str.length-i-1];
+    }
+  }
+  return +reverseStr;
 }
